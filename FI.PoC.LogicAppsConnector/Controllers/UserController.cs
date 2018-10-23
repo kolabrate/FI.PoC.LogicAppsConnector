46cd72e9b34e495d0274d8d99a48afa9682e9363
Anand Maran <admin@kolabrate.com.au>
2018-10-23 16:08:21 +1100

    include - definitions

---
+++ FI.PoC.LogicAppsConnector/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+
+namespace FI.PoC.LogicAppsConnector.Controllers
+{
+    public class UserController : ApiController
+    {
+        // POST: api/User
+        // Code needs to written - AAD
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+
+        // Notify: api/User/5
+        [HttpPost]
+        [Route("api/notify")]
+        public void Notify()
+        {
+            
+        }
+    }
+}
---
+++ FI.PoC.LogicAppsConnector/Models/notify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FI.PoC.LogicAppsConnector.Models
+{   
+    public class Notify
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
