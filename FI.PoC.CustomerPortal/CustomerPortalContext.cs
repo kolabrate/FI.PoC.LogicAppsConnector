13acd38357a21a6c94a1f8a9533d07666b0b5b92
admin <admin@ANANDMARAN-RDS>
2018-10-23 21:52:04 +1100

    Included EF + StartupAuth

---
+++ FI.PoC.CustomerPortal/CustomerPortalContext.cs
@@ -0,0 +1,31 @@
+namespace FI.PoC.CustomerPortal
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class CustomerPortalContext : DbContext
+    {
+        // Your context has been configured to use a 'CustomerPortalContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'FI.PoC.CustomerPortal.CustomerPortalContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'CustomerPortalContext' 
+        // connection string in the application configuration file.
+        public CustomerPortalContext()
+            : base("name=CustomerPortalContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
--- FI.PoC.CustomerPortal/Startup.cs
+++ FI.PoC.CustomerPortal/Startup.cs
@@ -1,18 +1,62 @@
 ﻿using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Cookies;
+using System.Configuration;
+using System.IdentityModel;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Tokens;
+using System.Threading.Tasks;
 using Owin;
-
 
 [assembly: OwinStartup(typeof(FI.PoC.CustomerPortal.Startup))]
 namespace FI.PoC.CustomerPortal
 {
     public class Startup
     {
-        string Authority = "https://login.microsoftonline.com/common/";
+        private readonly string Authority = "https://login.microsoftonline.com/common/";
         public void Configuration(IAppBuilder app)
         {
+            app.SetDefaultSignInAsAuthenticationType (CookieAuthenticationDefaults.AuthenticationType);
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions {
+                ClientId = ConfigurationManager.AppSettings[""],
+                Authority = Authority,
+                ClientSecret = ConfigurationManager.AppSettings[""],
+                Notifications = new OpenIdConnectAuthenticationNotifications()
+                {
+                    RedirectToIdentityProvider = (context) =>
+                    {
+                        // This ensures that the address used for sign in and sign out is picked up dynamically from the request
+                        // this allows you to deploy your app (to Azure Web Sites, for example)without having to change settings
+                        // Remember that the base URL of the address used here must be provisioned in Azure AD beforehand.
+                        string appBaseUrl = context.Request.Scheme + "://" + context.Request.Host + context.Request.PathBase;
+                        context.ProtocolMessage.RedirectUri = appBaseUrl;
+                        context.ProtocolMessage.PostLogoutRedirectUri = appBaseUrl;
+                        return Task.FromResult(0);
+                    },
+                    // we use this notification for injecting our custom logic
+                    SecurityTokenValidated = (context) =>
+                    {
+                        // retriever caller data from the incoming principal
+                        string issuer = context.AuthenticationTicket.Identity.FindFirst("iss").Value;
+                        string UPN = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.Name).Value;
+                        string tenantID = context.AuthenticationTicket.Identity.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+                        // Code - Anand Maran - Use entity framework
+                        // the caller was neither from a trusted issuer or a registered user - throw to block the authentication flow
+                        throw new SecurityTokenValidationException();
+                        return Task.FromResult(0);
+                    },
+                    AuthenticationFailed = (context) =>
+                    {
+                        context.OwinContext.Response.Redirect("/Home/Error?message=" + context.Exception.Message);
+                        context.HandleResponse(); // Suppress the exception
+                        return Task.FromResult(0);
+                    }
+                }
 
-            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+
+                });
 
         }
     }
