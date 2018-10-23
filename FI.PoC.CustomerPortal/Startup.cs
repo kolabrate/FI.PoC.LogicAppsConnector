626b0c96462b706bc7701e3bb6accaa80779f972
Anand Maran <admin@kolabrate.com.au>
2018-10-23 17:12:51 +1100

    commit

--- FI.PoC.CustomerPortal/Startup.cs
+++ FI.PoC.CustomerPortal/Startup.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Owin;
+
 
 [assembly: OwinStartup(typeof(FI.PoC.CustomerPortal.Startup))]
 namespace FI.PoC.CustomerPortal
 {
     public class Startup
     {
+        string Authority = "https://login.microsoftonline.com/common/";
         public void Configuration(IAppBuilder app)
         {
-            
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
         }
     }
 }
